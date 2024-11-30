08aa474884589ce2269108ea5f1e87d487b30179
FerchoXD <213524@ids.upchiapas.edu.mx>
2024-11-30 10:32:59 -0600

    Agregando detalles al home screen

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,6 +1,12 @@
+import 'package:dio/dio.dart';
+import 'package:fixnow/config/config.dart';
+import 'package:fixnow/domain/entities/supplier.dart';
 import 'package:fixnow/presentation/providers/auth/auth_provider.dart';
 import 'package:fixnow/presentation/screens.dart';
 import 'package:fixnow/presentation/widgets.dart';
+import 'package:fixnow/presentation/widgets/customer/SupplierSection.dart';
+import 'package:fixnow/presentation/widgets/customer/SupplierService.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -9,23 +15,7 @@ class HomeScreen extends ConsumerWidget {
   final int pageIndex;
   final List suppliersFound;
 
-  final viewRoutesCustomer = <Widget>[
-    const HomeView(),
-    const CommunityScreen(),
-    const AssistantScreen(),
-    const NotificationsScreen(),
-    const PrivateCustomerProfile()
-  ];
-
-  final viewRoutesSupplier = <Widget>[
-    const HomeView(),
-    const CommunityScreen(),
-    const NotificationsScreen(),
-    const FinanceScreen(),
-    const PrivateProfileSuplier()
-  ];
-
-   HomeScreen({
+  const HomeScreen({
     super.key,
     required this.pageIndex,
     this.suppliersFound = const [],
@@ -35,41 +25,66 @@ class HomeScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final colors = Theme.of(context).colorScheme;
     final authState = ref.watch(authProvider);
+
+    if (kDebugMode) {
+      print("suppliers desde HomeScreen: $suppliersFound");
+    }
+
+    final viewRoutesCustomer = <Widget>[
+      HomeView(suppliersFound: suppliersFound),
+      const CommunityScreen(),
+      const AssistantScreen(),
+      const NotificationsScreen(),
+      const PrivateCustomerProfile(),
+    ];
+
+    final viewRoutesSupplier = <Widget>[
+      HomeView(suppliersFound: suppliersFound),
+      const CommunityScreen(),
+      const AssistantScreen(),
+      const NotificationsScreen(),
+      const PrivateProfileSuplier(),
+    ];
+
     return Scaffold(
+      backgroundColor: colors.surface,
+      drawer: const SideMenu(),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              context.push('/history');
+            },
+            icon: const Icon(Icons.history),
+          )
+        ],
         backgroundColor: colors.surface,
-        drawer: const SideMenu(),
-        appBar: AppBar(
-          actions: [
-            IconButton(
-                onPressed: () {
-                  context.push('/history');
-                },
-                icon: const Icon(Icons.history))
-          ],
-          backgroundColor: colors.surface,
-        ),
-        body: IndexedStack(
-          index: pageIndex,
-          children: authState.user!.role == 'CUSTOMER'
-              ? viewRoutesCustomer
-              : viewRoutesSupplier,
-        ),
-        bottomNavigationBar: CustomBottomNavigation(currentIndex: pageIndex));
+      ),
+      body: IndexedStack(
+        index: pageIndex,
+        children: authState.user!.role == 'CUSTOMER'
+            ? viewRoutesCustomer
+            : viewRoutesSupplier,
+      ),
+      bottomNavigationBar: CustomBottomNavigation(currentIndex: pageIndex),
+    );
   }
 }
 
 class HomeView extends ConsumerWidget {
-  const HomeView({super.key});
+  final List<dynamic> suppliersFound;
+
+  const HomeView({super.key, required this.suppliersFound});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final colors = Theme.of(context).colorScheme;
     final authState = ref.watch(authProvider);
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: RefreshIndicator(
         onRefresh: () async {
-          // Llamar al método para actualizar los datos
           await _refreshData(ref);
         },
         child: SingleChildScrollView(
@@ -134,9 +149,11 @@ class HomeView extends ConsumerWidget {
   }
 
   Future<void> _refreshData(WidgetRef ref) async {
+
     print("Datos actualizados");
   }
 }
+
 
 class ServicesSection extends StatelessWidget {
   const ServicesSection({super.key});
@@ -173,52 +190,201 @@ class ServicesSection extends StatelessWidget {
             icon: const Icon(Icons.filter_list),
           ),
         ),
+        const SizedBox(height: 20),
+        const Text(
+          'Proveedores destacados',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 20),
+
+        FutureBuilder(
+  future: fetchSuppliers(),
+  builder: (context, snapshot) {
+    if (snapshot.connectionState == ConnectionState.waiting) {
+      return const Center(child: CircularProgressIndicator());
+    } else if (snapshot.hasError) {
+      return Center(
+        child: Text('Error: ${snapshot.error}'),
+      );
+    } else if (snapshot.data!.isEmpty) {
+      return const Center(
+        child: Text('No se encontraron proveedores destacados.'),
+      );
+    } else {
+      final suppliers = snapshot.data as List<Map<String, dynamic>>;
+      return ListView.builder(
+        shrinkWrap: true,
+        itemCount: suppliers.length,
+        itemBuilder: (context, index) {
+          final supplier = suppliers[index];
+          return ProviderCard(
+            name: supplier['fullname'],
+            profession: (supplier['selectedservices'] ?? []).join(', '),
+            imageUrl: supplier['image']['images'],
+            rating: supplier['relevance']?.toDouble() ?? 0.0,
+            price: supplier['standardprice']?.toDouble() ?? 0.0,
+            price2: supplier['hourlyrate']?.toDouble() ?? 0.0,
+          );
+        },
+      );
+    }
+  },
+)
+
       ],
+    );
+  }
+
+  Future<List<Map<String, dynamic>>> fetchSuppliers() async {
+    final service = ServiceSupplier();
+    final result = await service.fetchSuppliers('Electricidad');
+
+    if (result['status'] == 200) {
+      return List<Map<String, dynamic>>.from(result['data']);
+    } else {
+      print('Error: ${result['message']}');
+      throw Exception(result['message']);
+    }
+  }
+}
+
+class ProviderCard extends StatelessWidget {
+  final String name;
+  final String profession;
+  final String imageUrl;
+  final double rating;
+  final double price;
+  final double price2;
+
+  const ProviderCard({
+    super.key,
+    required this.name,
+    required this.profession,
+    required this.imageUrl,
+    required this.rating,
+    required this.price,
+    required this.price2,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    print("imageUrl: $imageUrl");
+    print("name: $name");
+    print("profession: $profession");
+    print("rating: $rating");
+    print("price: $price");
+
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.network(
+              imageUrl,
+              width: 50,
+              height: 50,
+              fit: BoxFit.cover,
+              errorBuilder: (context, error, stackTrace) {
+                return const Icon(
+                  Icons.person,
+                  size: 50,
+                  color: Colors.grey,
+                );
+              },
+            ),
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                Text(
+                  profession,
+                  style: const TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(width: 10),
+          Column(
+            children: [
+              Text(
+                price != null && price != 0 // Si `price` no es nulo ni cero
+                    ? '\$$price'
+                    : price2 != null && price2 != 0 // Si `price2` no es nulo ni cero
+                        ? '\$$price2'
+                        : 'Negociable',
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Row(
+                children: [
+                  const Icon(Icons.star, color: Colors.yellow, size: 16),
+                  Text(rating == 0 ? 'Nuevo' : rating.toStringAsFixed(1)),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
 
 class ProvidersSection extends StatelessWidget {
-  const ProvidersSection({super.key});
+  final List<dynamic> suppliersFound;
+
+  const ProvidersSection({super.key, required this.suppliersFound});
 
   @override
   Widget build(BuildContext context) {
+    for (var element in suppliersFound) {
+      if (kDebugMode) {
+        print("suppliers desde found equisde{$element['fullname']}");
+      }
+    }
+
+    if (suppliersFound.isEmpty) {
+      return const Center(
+        child: Text('No se encontraron proveedores.'),
+      );
+    }
+
     return Column(
-      children: [
-        GestureDetector(
+      children: suppliersFound.map((supplier) {
+        if (kDebugMode) {
+          print("suppliers desde found equisde{$supplier['fullname']}");
+        }
+        return GestureDetector(
           onTap: () {
-            context.push('/supplier/profile');
+            context.push('/supplier/profile', extra: supplier);
           },
           child: ProviderCard(
-            name: 'Fernando Daniel',
-            profession: 'Carpintero',
-            price: 350,
-            rating: 4,
+            name: supplier['fullname'] ?? 'Desconocido',
+            profession:
+                supplier['selectedservices']?.join(', ') ?? 'Sin profesión',
+            imageUrl: supplier['image']['images'] ?? '',
+            price: supplier['hourlyrate'],
+            price2: supplier['standardprice'] ?? 0,
+            rating: supplier['relevance'] ?? 0,
           ),
-        ),
-        GestureDetector(
-          onTap: () {
-            context.push('/supplier/profile');
-          },
-          child: ProviderCard(
-            name: 'Fernando Daniel',
-            profession: 'Fontanero',
-            price: 350,
-            rating: 3,
-          ),
-        ),
-        GestureDetector(
-          onTap: () {
-            context.push('/supplier/profile');
-          },
-          child: ProviderCard(
-            name: 'Fernando Daniel',
-            profession: 'Electricista',
-            price: 350,
-            rating: 5,
-          ),
-        ),
-      ],
+        );
+      }).toList(),
     );
   }
 }
